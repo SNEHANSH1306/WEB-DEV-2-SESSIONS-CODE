e1ab591c268e0e58b050dadcd52c49a348aa03d9
SNEHANSH1306 <snehanshsnehu@gmail.com>
2026-01-21 15:41:04 +0530

    Day 3 Code

---
+++ DAY 3/script.js
@@ -0,0 +1,56 @@
+// let sentence = "I am a comma separated sentence";
+// let words = sentence.split(" ");
+// console.log(words)
+
+// let sentence2 = "I am a comma separated sentence";
+// let words2 = sentence2.split("a");
+// console.log(words2)
+
+
+// // Q1. Find out the occurance of "r" in the given string.
+
+//     let str = "Tu meri mai tera mai tera tu meri"
+//     let count = str.split("r").length-1;
+//     console.log(count);
+
+    
+// Regular Function
+    // function demo(){
+    //     console.log("demo")
+    // }
+    // demo()
+
+    // function sample(a,b){
+    //     console.log(a+b)
+    // }
+    // sample(1,2)
+
+    // function sample(a,b){
+    //     return a+b;
+    // }
+    // console.log(sample(1,2))
+
+// Arrow Function
+// Advantage: doesn't need return and function keyword and it will perform asynchronously.
+    // const test=(c,d)=>c+d;
+    // console.log(test(32,42));
+
+// Anonymous Fuction
+
+    // setTimeout(callback,timedelay);
+
+    // setTimeout(()=>{
+    //     alert("please Sign in")
+    // },5000);
+    
+    // setInterval(()=>{
+    //     alert("sign Up")
+    // },2000);
+
+    // let date = new Data();
+    // console.log(data)
+
+// Self Invoking Function
+    // (function(){
+    //     console.log("demo")
+    // })()
