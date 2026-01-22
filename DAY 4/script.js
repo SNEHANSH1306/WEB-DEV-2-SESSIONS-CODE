b0364fa1da493c05bde58166d8094cbaea0c3e2e
SNEHANSH1306 <snehanshsnehu@gmail.com>
2026-01-22 11:45:51 +0530

    Day 4 Javascript code

---
+++ DAY 4/script.js
@@ -0,0 +1,16 @@
+// let user = {
+//     FullName:"SNEHANSH JANGIR",
+//     address:"Badshahpur",
+//     mobile_no:1234567890,
+//     favcolor:["Blue","White","Red"]
+// };
+// console.log(user["FullName"]); // also can be accessed by this syntax.
+// We can access the individual value by targeting key with variable name.
+
+// console.log(user.FullName,user.favcolor,user.mobile_no);
+
+
+// let date = new Date()
+// console.log(date)
+
+
