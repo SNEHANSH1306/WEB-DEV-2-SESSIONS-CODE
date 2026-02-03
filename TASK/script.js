3e9383033ed8419b3612d7d590de061360cea5da
SNEHANSH1306 <snehanshsnehu@gmail.com>
2026-02-03 12:33:40 +0530

    TASKS 3, 4 , 5

---
+++ TASK/script.js
@@ -0,0 +1,57 @@
+// TASK 1
+
+    // const profile = document.getElementById("profile");
+    // profile.style.backgroundColor = 'aqua';
+    // profile.style.padding = '15px';
+    // profile.style.textAlign = 'Center'
+
+// TASK 2
+
+    // const highlight = document.getElementsByClassName("important");
+    // console.log(highlight)
+    // for(let i = 0;i<highlight.length; i++){
+        
+    //     highlight[i].style.color = 'Red';
+    //     highlight[i].style.fontSize = '50px';
+    // }
+    
+    
+    // console.log(highlight.length)
+
+
+// TASK 3
+
+    // const task3 = document.getElementsByTagName("p");
+    // console.log(task3);
+
+    // for(let i = 0;i < task3.length; i++){
+    //     if(i%2===0){
+    //         task3[i].style.color = 'Blue';
+    //     }
+    //     else{
+    //         task3[i].style.color = 'Green';
+    //     }
+    // }
+
+    // task3[task3.length-1].style.fontWeight = "bold";
+
+// TASK 4
+
+    // const task4 = document.querySelectorAll(".box");
+    // for(let i = 0; i < task4.length; i++){
+    //     task4[i].style.backgroundColor = "cyan";
+    // }
+    // task4.textContent = "I am First Box";
+
+// TASK 5
+
+    // const section = document.getElementById("content");
+    // console.log(section.textContent);
+    // const task5 = section.querySelectorAll("p")
+
+    // for(let i = 0; i < task5.clientHeight;i++){
+    //     task5[i].style.color = "purple";
+    // }
+
+
+
