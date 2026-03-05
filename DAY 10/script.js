d3e38dd09a84e2122de2af9ad0ac8a599ecb865a
SNEHANSH1306 <snehanshsnehu@gmail.com>
2026-03-05 11:57:04 +0530

    ASYNCHRONOUS AND SYNCHRONOUS:

--- DAY 10/script.js
+++ DAY 10/script.js
@@ -1,29 +1,30 @@
-var a=10;
-console.log(a);
-function demo(){
-    let b=20;
-    console.log(b)
-}
-demo()
+// var a=10;
+// console.log(a);
+// function demo(){
+//     let b=20;
+//     console.log(b)
+// }
+// demo()
 
 //callback hell
-setTimeout(()=>{
-    console.log("a");
-    setTimeout(()=>{
-        console.log("b");
-        setTimeout(()=>{
-            console.log("c");
-            setTimeout(()=>{
-                console.log("d");
-                setTimeout(()=>{
-                    console.log("e")
-                },1000)
-            },1000)
-        },1000)
-    },1000)
-},1000)
+    // setTimeout(()=>{
+    //     console.log("a");
+    //     setTimeout(()=>{
+    //         console.log("b");
+    //         setTimeout(()=>{
+    //             console.log("c");
+    //             setTimeout(()=>{
+    //                 console.log("d");
+    //                 setTimeout(()=>{
+    //                     console.log("e")
+    //                 },1000)
+    //             },1000)
+    //         },1000)
+    //     },1000)
+    // },1000)
 
 //promises
+
 let pro = new Promise((resolve,reject)=>{
     let proposal="false"
     if(proposal==="true"){
@@ -35,3 +36,23 @@ let pro = new Promise((resolve,reject)=>{
 })
 pro.then(()=>console.log("proposal accepted"))
 .catch(()=>console.log("proposal rejected"))
+
+// Same code woth function syntax
+
+function pro(){
+return new Promise((resolve,reject)=>{
+    let proposal="false"
+    if(proposal==="true"){
+        resolve()
+    }
+    else{
+        reject()
+    }
+})
+}
+pro().then(()=>console.log("proposal accepted"))
+.catch(()=>console.log("proposal rejected"))
+.finally(()=>console.log("Will run everytime when promise called"))
+
+// Event loop: It is a constantly running process that checks the call stack and the task queue. If the call stack is empty, it takes the first task from the task queue and pushes it to the call stack.
+
